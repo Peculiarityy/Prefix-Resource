7f57d2855a141a39629b2521488737cacafaf05a
ShadyPaws <malxaniza@gmail.com>
2017-09-04 20:18:44 -0700

    Basic Commit
    Just uploading the resource to the website

---
+++ src/plugin/pecularityy/main/Main.java
@@ -0,0 +1,22 @@
+package plugin.pecularityy.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/*
+ * Created by ShadyCarpet
+ * PrefixPlugin created in 9/4/2017
+ * All work belongs to ShadyCarpet
+ */
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+        getLogger().info("Prefix plugin created by -- Pecularityy Enabled!");
+
+
+    }
+
+
+
+}
